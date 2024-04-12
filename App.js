f1d5d3d7be64e2ba00e6b282b648edac47bf1086
Mateus B. Reck <mateus8reck@hotmail.com>
2024-04-11 22:14:59 -0300

    add banco de dados

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Cadastro from './src/componentes/Cadastro';
 import Login from './src/componentes/Login';
 import Chat from './src/componentes/Chat';
+import db from './src/componentes/servivices/firebaseConfi';
 
 const Stack = createStackNavigator();
 
 const App = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Cadastro">
+            <Stack.Navigator initialRouteName="Login">
                 <Stack.Screen name="Cadastro" component={Cadastro} />
                 <Stack.Screen name="Login" component={Login} />
                 <Stack.Screen name="Chat" component={Chat} />
